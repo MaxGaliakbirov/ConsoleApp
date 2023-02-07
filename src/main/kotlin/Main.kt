64f1bc4a07faf964d0a497f2ffe009c2746d4d75
Max Galiakbirov <mf.galiakbirov@gmail.com>
2023-02-07 10:07:19 +0300

    ввод и вывод, но есть проблема, после считывания второй комманды, "приложение" схлопывается

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 
 val scan = java.util.Scanner(System.`in`)
 fun main(args: Array<String>) {
-    println("Для начала работы введите комманду /start. \"\\n\" Укажите через пробел")
+    val startCommand = scan.next()
+    if (startCommand != "/start" || startCommand != "/help") {
+        println("Вы ввели не корректную комманду, наберите /help  для вывода списка комманд")
+        scan.next()
+    }
+    else if (startCommand == "/start") {
+        println("Добро пожаловать!")
+        scan.next()
+    }
+    else if (startCommand == "/help") {
+        println("Список команд...")
+        scan.next()
+    } else if (startCommand == "/end") {
+        println("Всего доброго!")
+    }
 }
