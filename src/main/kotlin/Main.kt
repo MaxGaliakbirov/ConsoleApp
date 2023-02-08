f6560316c4abb8f21afd425366f441452369f37e
Max Galiakbirov <mf.galiakbirov@gmail.com>
2023-02-08 18:15:58 +0300

    готовое задание №1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,18 @@
 
-val scan = java.util.Scanner(System.`in`)
+//val scan = java.util.Scanner(System.`in`)
 fun main(args: Array<String>) {
-    val startCommand = scan.next()
-    if (startCommand != "/start" || startCommand != "/help" || startCommand != "/end") {
-        println("Вы ввели не корректную комманду, наберите /help  для вывода списка комманд")
-        scan.next()
-    }
-    else if (startCommand == "/start") {
-        println("Добро пожаловать!")
-        scan.next()
-    }
-    else if (startCommand == "/help") {
-        println("Список команд...")
-        scan.next()
-    } else if (startCommand == "/end") {
-        println("Всего доброго!")
+    println("Для начала работы введите комманду /start.")
+
+    while (true) {
+        val startCommand = readLine()
+        when (startCommand) {
+            "/start" -> println("Добро пожаловать!")
+            "/help" -> println("Список команд: \n /start - начало работы программы.\n /end - завершение программы")
+            "/end" -> {
+                println("Всего доброго!")
+                break
+            }
+            else -> println("Вы ввели не корректную команду.  введите /help для вывода доступных комманд")
+        }
     }
 }
